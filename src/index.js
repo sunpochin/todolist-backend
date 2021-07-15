6e30425515027426f8c25b9b6c1bfd035dc17028
sun pochin <sunpochin@gmail.com>
2021-07-15 14:31:44 +0800

    test port with example

--- src/index.js
+++ src/index.js
@@ -12,14 +12,23 @@ const httpsOptions = {
 	key: fs.readFileSync("./private-files/certificates/yesido_me.key", "utf8"),
 };
 
-const httpsServer = https.createServer(httpsOptions, app);
-const httpServer = http.createServer(app);
-
-const httpsPort = process.env.PORT || 8082;
-const httpPort = process.env.PORT || 8081;
+// const httpsServer = https.createServer(httpsOptions, app);
+// const httpServer = http.createServer(app);
+//
+// // const httpsPort = process.env.PORT || 8082;
+// const httpPort = process.env.PORT;
 // httpServer.listen(httpPort, () => {
 // 	console.log("httpPort: ", httpPort);
+//
 // });
-httpsServer.listen(httpsPort, () => {
-	console.log("httpSecure Port: ", httpsPort);
+// httpsServer.listen(httpsPort, () => {
+// 	console.log("httpSecure Port: ", httpsPort);
+// });
+
+http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-type', 'application/json');
+	res.end('elastic beanstalk');
+}).listen(process.env.PORT, () => {
+	console.log("listen on : ", process.env.PORT);
 });
