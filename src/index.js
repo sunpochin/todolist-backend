f713e10a48085ca4b166d26459ca822fad52cfcb
sun pochin <sunpochin@gmail.com>
2021-07-15 15:09:42 +0800

    fix port on elasticbean 3

--- src/index.js
+++ src/index.js
@@ -7,27 +7,37 @@ const fs = require("fs");
 const app = require("./app");
 
 const httpsOptions = {
-	ca: fs.readFileSync("./private-files/certificates/multi_498484707.ca-bundle", "utf8"),
-	cert: fs.readFileSync("./private-files/certificates/multi_498484707.crt", "utf8"),
-	key: fs.readFileSync("./private-files/certificates/yesido_me.key", "utf8"),
+  ca: fs.readFileSync("./private-files/certificates/multi_498484707.ca-bundle", "utf8"),
+  cert: fs.readFileSync("./private-files/certificates/multi_498484707.crt", "utf8"),
+  key: fs.readFileSync("./private-files/certificates/yesido_me.key", "utf8"),
 };
 
-// const httpsServer = https.createServer(httpsOptions, app);
+const httpsServer = https.createServer(httpsOptions, app);
 // const httpServer = http.createServer(app);
-const httpServer = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "application/json");
-  res.end("{app: elastic beanstalk}");
-});
-const httpPort = process.env.PORT || 8081;
-httpServer.listen(httpPort, () => {
-  console.log("listen on : ", httpPort);
-});
+// const httpServer = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-type", "application/json");
+//   res.end({
+//     "port": "http"
+//   });
+// });
+// const httpsServer = https.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-type", "application/json");
+//   res.end({
+//     "port": "secure port"
+//   });
+// });
+
+const httpsPort = process.env.PORT || 8082;
+// secureServer.listen(httpsPort, () => {
+//   console.log("listen on : ", httpsPort);
+// });
 
 // const httpsPort = process.env.PORT || 8082;
 // httpServer.listen(httpPort, () => {
 // 	console.log("httpPort: ", httpPort);
 // });
-// httpsServer.listen(httpsPort, () => {
-// 	console.log("httpSecure Port: ", httpsPort);
-// });
+httpsServer.listen(httpsPort, () => {
+	console.log("httpSecure Port: ", httpsPort);
+});
